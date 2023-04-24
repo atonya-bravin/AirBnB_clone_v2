b6e622e640a5d72264db9fcffb366336514e1db9
bravin-atonya <bravinatonya.brani@gmail.com>
2023-04-24 01:08:06 +0000

    /c/<text>_ display 'C ' followed by the value of the text variable (replace underscore _ symbols w ith a space)

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -15,10 +15,12 @@ def hbnb():
     """ Prints a Message when /hbnb is called """
     return 'HBNB'
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def c_is_fun(text):
     """ Prints a Message when /c is called """
     return "C " + text.replace('_', ' ')
+
 
 if __name__ == "__main__":
     """ Main Function """
