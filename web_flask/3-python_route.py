a2bd05c97214c96c0a21ce811867c032bbe175fd
bravin-atonya <bravinatonya.brani@gmail.com>
2023-04-24 01:17:21 +0000

    /python/(<text>): display 'Python' , followed by the value of the text variable (replace underscore _ symbols with a space)

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -21,6 +21,7 @@ def c_is_fun(text):
     """ Prints a Message when /c is called """
     return "C " + text.replace('_', ' ')
 
+
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_is_cool(text='is_cool'):
