2b6b9b48e675b85ae56f1938873818f2017fe97d
bravin-atonya <bravinatonya.brani@gmail.com>
2023-04-24 00:53:17 +0000

    starts a Flask web application listenning on 0.0.0.0, port 5000

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,8 +6,9 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-   return 'Hello HBNB!'
+    return 'Hello HBNB!'
 
-   if __name__ == "__main__":
-      """ Main Function """
-      app.run(host='0.0.0.0', port=5000)
+
+if __name__ == "__main__":
+    """ Main Function """
+    app.run(host='0.0.0.0', port=5000)
